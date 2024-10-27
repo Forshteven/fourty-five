a70e6eaf20915d666e8a49fb02c15683553fa4ff
Forshteven <geocinds@mail.ru>
2024-10-27 14:02:41 +0300

    Add files via upload

--- module_9_7.py
+++ module_9_7.py
@@ -1,23 +1,23 @@
 def is_prime(sum_three):
     def wrapper(*args, **kwargs):
         result = sum_three(*args, **kwargs)
-        if result < 2:
-            print("Составное")
+        if result % 2 == 0 or result % 3 == 0:
+            print('Составное')
+
+        elif result % result ** 0.5 == 0:
+            print('Составное')
+
+        else:
+            print('Простое')
             return result
 
-        for i in range(2, int(result ** 0.5) + 1):
-            if result % i == 0:
-                print("Составное")
-                return result
-
-        print("Простое")
-        return result
     return wrapper
 
+
 @is_prime
-def sum_three(a,b,c):
+def sum_three(a, b, c):
     return a + b + c
 
 
-result = sum_three(6, 3, 6)
+result = sum_three(9, 3, 6)
 print(result)
